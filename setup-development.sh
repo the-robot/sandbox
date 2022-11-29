f31a8a232c775f758f6126aa2380c9af74a7f745
Gigachad <9334746+the-robot@users.noreply.github.com>
2022-11-29 19:04:34 +0800

    Update setup-development.sh

--- setup-development.sh
+++ setup-development.sh
@@ -24,11 +24,12 @@ echo "[+] Installed pnpm."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -s -o "/tmp/awscliv2.zip"
 unzip /tmp/awscliv2.zip -d /tmp
 sudo /tmp/aws/install
+echo "[+] Installed AWS CLI. Please configure aws-cli later."
 
 # setup Zerotier network
 curl -s https://install.zerotier.com -s | sudo bash
 sudo service zerotier-one start
-echo "[+] Installed and configured Zerotier Network."
+echo "[+] Installed and configured Zerotier network."
 sudo service zerotier-one status
 
 # generate SSH key to add to your github account
