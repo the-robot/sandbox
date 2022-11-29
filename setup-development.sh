9a0ef73bfcfbd833d7bf139adb4093067031f7a9
Gigachad <9334746+the-robot@users.noreply.github.com>
2022-11-29 16:02:23 +0000

    feat: use tailscale instead of zerotier

--- setup-development.sh
+++ setup-development.sh
@@ -27,10 +27,20 @@ sudo /tmp/aws/install
 echo "[+] Installed AWS CLI. Please configure aws-cli later."
 
 # setup Zerotier network
-curl -s https://install.zerotier.com -s | sudo bash
-sudo service zerotier-one start
-echo "[+] Installed and configured Zerotier network."
-sudo service zerotier-one status
+# curl -s https://install.zerotier.com -s | sudo bash
+# sudo service zerotier-one start
+# echo "[+] Installed and configured Zerotier network."
+# sudo service zerotier-one status
+# cp ./scripts/init-zerotier ~/.local/bin
+# echo "[+] When you restart the codespace, run `init-zerotier` to run services like Zerotier."
+
+# setup Tailscale network
+wget https://pkgs.tailscale.com/stable/tailscale_1.32.3_amd64.tgz -O /tmp/tailscale.tgz
+tar -xf /tmp/tailscale.tgz --directory ~/.local
+mv ~/.local/tailscale_1.32.3_amd64 ~/.local/tailscale
+ln -s ~/.local/tailscale/tailscale ~/.local/bin
+ln -s ~/.local/tailscale/tailscaled ~/.local/bin
+echo "[+] Run `tailscaled` in background to connect to tailscale network."
 
 # generate SSH key to add to your github account
 read -p 'Enter email for ssh key: ' email
@@ -39,7 +49,3 @@ echo "[+] Generated SSH key."
 
 echo "[!] Copy the public key to GitHub settings."
 cat ~/.ssh/id_rsa.pub
-
-# copy custom scripts to ~/.local/bin
-cp ./scripts/init-sandbox ~/.local/bin
-echo "[+] When you restart the codespace, run `init-sandbox` to run services like Zerotier."
