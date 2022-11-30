b5edb2abde21374990e8f4e0f916fe8c13534a3e
Gigachad <9334746+the-robot@users.noreply.github.com>
2022-11-30 09:22:21 +0800

    Update setup-kali.sh

--- setup-kali.sh
+++ setup-kali.sh
@@ -4,7 +4,8 @@ echo "[!] Only run this after setup Kali repositories using Katoolin."
 echo "https://github.com/LionSec/katoolin#installation"
 
 # install kali tools
-sudo apt install wpscan sqlmap hashcat seclists nmap hydra tcpdump netcat wordlists steghide impacket-scripts dirbuster dirb ffuf wfuzz gobuster feroxbuster sublist3r traceroute whois cewl recon-ng mimikatz hash-identifier dsniff dnsmap dnsrecon crackmapexec dirsearch dnsenum sslscan fcrackzip python3-impacket -y -qq
+sudo apt install exploitdb metasploit-framework wpscan sqlmap hashcat seclists nmap hydra tcpdump netcat wordlists steghide impacket-scripts dirbuster dirb ffuf wfuzz gobuster feroxbuster sublist3r traceroute whois cewl recon-ng mimikatz hash-identifier dsniff dnsmap dnsrecon crackmapexec dirsearch dnsenum sslscan fcrackzip python3-impacket -y -qq
+sudo apt autoremove
 
 # install rustscan
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb -q -O /tmp/rustscan.deb
